9b37330d5b3a9be45f0685f847f372d2a04bcc6c
levan sardalashvili <sardalaalevani@gmail.com>
2022-01-24 18:36:06 +0400

    app port

---
+++ src/data.js
@@ -0,0 +1,63 @@
+export const products = [
+  {
+    id:1,
+    img: "https://miro.medium.com/max/5680/1*vM5yDKfcgjNgEZrYzmdO4A.png",
+    link: "https://levansardalashvili.github.io/weather-app/",
+  },
+  {
+    id:2,
+    img: "https://food.bolt.eu/images/phones-with-bolt-food.jpg",
+    link: "https://levansardalashvili.github.io/food-app/",
+  },
+  {
+    id:3,
+    img: "https://koenig-media.raywenderlich.com/uploads/2017/03/MobileAppSearch-feature.png",
+    link: "https://levansardalashvili.github.io/react-search-image-app/",
+  },
+  {
+    id: 4,
+    img: "https://www.logodee.com/wp-content/uploads/2020/10/LD-C-14-1.jpg",
+    link: "https://levansardalashvili.github.io/building-house-react-app/",
+  },
+  {
+    id: 5,
+    img: "https://btl-company.com/wp-content/uploads/2019/06/Business-travel.jpg",
+    link: "https://levansardalashvili.github.io/travel-react-app/",
+  },
+  {
+    id: 6,
+    img: "https://www.getwox.com/wp-content/uploads/2020/12/Movie-HD.jpeg.webp",
+    link: "https://levansardalashvili.github.io/movies-app/",
+  },
+  {
+    id: 7,
+    img: "https://images.unsplash.com/photo-1539720772528-faa7cd679bcd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDEwfHx8ZW58MHx8fHw%3D&w=1000&q=80",
+    link: "https://dreamy-payne-b20cdc.netlify.app/",
+
+  },
+  {
+    id: 8,
+    img: "https://media.istockphoto.com/photos/three-glasses-of-white-rose-and-red-wine-on-a-wooden-barrel-picture-id1301017778?b=1&k=20&m=1301017778&s=170667a&w=0&h=yyBcy8u5AXTHSlfkSsuUIUSZlaVFU9VmTWitFJJE2y8=",
+    link: "https://infallible-swirles-f32bb4.netlify.app/",
+  },
+  {
+    id: 9,
+    img: "https://img.i-scmp.com/cdn-cgi/image/fit=contain,width=1098,format=auto/sites/default/files/styles/1200x800/public/d8/images/canvas/2021/10/15/78cdc3a1-bf34-4e63-898f-616aa9357471_e44f733d.jpg?itok=wDOx-uc4&v=1634303365",
+    link: "https://naughty-bhaskara-b540c0.netlify.app/",
+  },
+  {
+    id: 10,
+    img: "https://www.intelligenthq.com/wp-content/uploads/2018/08/Industrial-Robotics-AI-Machine-Learning.jpeg",
+    link: "https://hopeful-kilby-7c985f.netlify.app/",
+  },
+  {
+    id: 11,
+    img: "https://res.cloudinary.com/duu3v9gfg/image/fetch/t_fit_1920/https://78884ca60822a34fb0e6-082b8fd5551e97bc65e327988b444396.ssl.cf3.rackcdn.com/up/2021/10/60-1_MARA9067-1633542211-1633542211.jpg",
+    link: "https://jolly-morse-be60e1.netlify.app/",
+  },
+  {
+    id: 12,
+    img: "https://img4.goodfon.com/wallpaper/nbig/e/5c/computer-codding-coding-programing.jpg",
+    link: "https://jolly-morse-be60e1.netlify.app/",
+  }
+];
